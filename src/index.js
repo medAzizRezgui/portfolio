0ac3ac7034d3d2bb709bbb40322032b9b025f081
medAzizRezgui <49117884+medAzizRezgui@users.noreply.github.com>
2021-08-21 00:44:13 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import './styles/Ultimate-Config.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
